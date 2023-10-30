10781df2028d3b4f67a3de852ce81807fb021d8f
DelroyGayle <delroygayle01@gmail.com>
2023-10-30 19:00:52 +0000

    Remove 'http://' from the app name om ALLOWED_HOSTS

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://dg-codestar-2023-efda2e25c8ca.herokuapp.com', 
+ALLOWED_HOSTS = ['dg-codestar-2023-efda2e25c8ca.herokuapp.com', 
                 '8000-delroygayle-djangoblog-bkd0r41314o.ws-eu105.gitpod.io']
 
 
