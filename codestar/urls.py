b4c54861767d2230ca48bf68465c7b4af309889b
DelroyGayle <delroygayle01@gmail.com>
2023-11-03 11:07:21 +0000

    DEBUG = False

--- codestar/urls.py
+++ codestar/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('', include('blog.urls'), name='blog_urls'),
     path('accounts/', include('allauth.urls')),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
